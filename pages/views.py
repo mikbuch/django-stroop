5a91c8f2a0f68a46d2878a784aaacd971bd3c448
mikbuch <mikolaj.buchwald@gmail.com>
2019-02-11 19:55:33 +0100

    JavaScript working
    Keypress detetion and RT

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,15 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from django.utils.html import mark_safe
 import json
+
 
 def experiment(request):
 
-    parameters = {"zgodne": {"zielony": "green", "niebieski" : "blue"},
-                  "niezgodne": {"czerwone":"blue", "pstrokaty":"yellow"}}
+    parameters = {"zgodne": {"zielony": "green", "niebieski": "blue"},
+                  "niezgodne": {"czerwone": "blue", "pstrokaty": "yellow"}}
 
-    return render(request, 'experiment.html', {'parameters': json.dumps(parameters)})
-
+    return render(request, 'experiment.html', {'parameters':
+                                               json.dumps(parameters)})
 
 
 class HomePageView(TemplateView):
