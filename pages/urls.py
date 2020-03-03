08b568cf9725e46ee47a4cb5b5fde014035d3989
zorcix <zuzanna.n@onet.com.pl>
2020-03-03 20:55:52 +0100

    seria próbna

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, experiment
+from .views import HomePageView, experiment, pre_experiment
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('experiment', experiment, name='experiment'),
+    path('pre_experiment', pre_experiment, name='pre_experiment')
 ]
--- pages/views.py
+++ pages/views.py
@@ -12,6 +12,13 @@ def experiment(request):
     return render(request, 'experiment.html', {'parameters':
                                                json.dumps(parameters)})
 
+def pre_experiment(request):
+        parameters = {"zgodne": [{"zielony": "green"}, {"niebieski": "blue"}, {"czerwony": "red"}, {"żółty": "yellow"}],
+                      "niezgodne": [{"czerwony": "blue"},{"czerwony": "green"},{"czerwony": "yellow"}, {"niebieski": "yellow"}, {"niebieski": "red"}, {"niebieski": "green"}, {"żółty": "green"}, {"żółty": "red"}, {"żółty": "blue"}, {"zielony": "red"}, {"zielony": "yellow"}, {"zielony": "blue"}],
+                      "kontrolne": [{"xxxxxx": "red"}, {"xxxxxx": "green"}, {"xxxxxx": "blue"}, {"xxxxxx": "yellow"}]}
+
+        return render(request, 'pre_experiment.html', {'parameters':
+                                                   json.dumps(parameters)})
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
